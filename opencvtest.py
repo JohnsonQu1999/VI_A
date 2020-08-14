d9c73e50a29c5c83837104597ca8beeb22d20ce8
johnsonqu1999 <johnsonqu1999@gmail.com>
2020-08-14 18:56:09 -0400

    opencv test

---
+++ opencvtest.py
@@ -0,0 +1,22 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+
+if(cap.isOpened() == False):
+	print("Error opening video stream")
+
+while(cap.isOpened()):
+	ret, frame = cap.read()
+
+	if(ret == True):
+		cv2.imshow('Frame',frame)
+
+		if(cv2.waitKey(25) & 0xFF == ord('q')):
+			break;
+	else:
+		break;
+
+cap.release()
+
+cv2.destroyAllWindows()
